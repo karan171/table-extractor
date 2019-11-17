8c540f0dbb64b0317a246dbe3c71cefd9247f54b
karan171 <karanjuneja32@gmail.com>
2019-11-17 14:45:09 +0530

    Added Support for Tabula
    Support for tabula added in the tables.py file just add the dpi and the path and run the code to get the pdf output in csv.

--- table_detect/table_area.py
+++ table_detect/table_area.py
@@ -14,3 +14,17 @@ def get_table_area(image,output_dict,counter):
         im1 = image.crop((left,top,right,bottom))
         tables.append(im1)
     return tables
+
+
+def get_table_coords(image,output_dict,counter):
+    tables_coords = []
+    im_width, im_height = image.size
+    for i in range(counter):
+        ymin = output_dict['detection_boxes'][i][0]
+        xmin = output_dict['detection_boxes'][i][1]
+        ymax = output_dict['detection_boxes'][i][2]
+        xmax = output_dict['detection_boxes'][i][3]
+        (left, right, top, bottom) = (xmin * im_width, xmax * im_width,
+                                      ymin * im_height, ymax * im_height)
+        tables_coords.append((top,left,bottom,right))
+    return tables_coords
--- tables.py
+++ tables.py
@@ -1,17 +1,35 @@
 from pdf_to_image.image_pdf import get_images
 from table_detect.detection import get_tables
 from table_detect.table_area import get_table_area
+from table_detect.table_area import get_table_coords
 import matplotlib.pyplot as plt
+import tabula
 from PIL import Image
+import pandas as pd
 
 #Accept Input from user as a SinglePage PDF or a Normal Image.
 
+path = 'C:/Intellectics/images/2.pdf'
+dpi = 60
+image = get_images(path=path,dpi=dpi)
 
-image = get_images(path='C:/Intellectics/images/2.pdf',dpi=300)
-tables_area = get_tables(image)
-output_dic,counter = tables_area
-tables_image = get_table_area(image,output_dic,counter)
+
+def create_tables(image):
+    tables_area = get_tables(image)
+    output_dic,counter = tables_area
+    tables_image = get_table_area(image,output_dic,counter)  #Will be used when support for tesseract-OCR is added
+    tables_coords = get_table_coords(image,output_dic,counter)
+    # counter = 0
+    # for i in tables_image:
+    #     result = i.save("out{}.jpg".format(counter))
+    #     counter+=1
+    return tables_coords
+
+
+table_coords = create_tables(image)
 counter = 0
-for i in tables_image:
-    result = i.save("out{}.jpg".format(counter))
+for area in table_coords:
+    # tab_df = tabula.read_pdf(input_path=path,pages='all',spreadsheet=True,multiple_tables=True,area=area,encoding='utf-8')
+    tabula.convert_into(input_path=path,output_path='tables_out{}.csv'.format(counter),guess=False,area=area,pages='all')
     counter+=1
+
